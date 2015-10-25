ee13fadd1f0b49778f5b71666ec7a1220efbf879
Adam Uhlir <uhlir.a@gmail.com>
2015-10-26 00:01:26 +0100

    Menu tweaking

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,21 @@ var menuHandler = function (index, nextIndex, direction) {
     var nav = $('nav');
     nextIndex--;
 
+    var duration;
+    if(direction == 'up'){
+        duration = 100;
+    }else{
+        duration = 480;
+    }
+
     if(!framesMenuClasses[nextIndex]){
         setTimeout(function () {
-            nav.removeClass();
-        }, 300);
+            nav.removeAttr('class');
+        }, duration);
     }else if(!nav.hasClass(framesMenuClasses[nextIndex])){
         setTimeout(function () {
-            nav.removeClass().addClass(framesMenuClasses[nextIndex]);
-        }, 300);
+            nav.removeAttr('class').addClass(framesMenuClasses[nextIndex]);
+        }, duration);
     }
 };
 
